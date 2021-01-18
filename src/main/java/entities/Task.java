9bb479832796a381a1ed41049a3dcaf91f733d0a
Pelle Rasmussen <cph-pr128@cphbusiness.com>
2021-01-18 20:07:18 +0100

    added entity TaskType

---
+++ src/main/java/entities/Task.java
@@ -0,0 +1,62 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+public class Task implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    @Column(length = 255)
+    private String comment;
+
+    private String dueDate;
+
+    public Task() {
+    }
+
+    public Task(String title, String comment, String dueDate) {
+        this.title = title;
+        this.comment = comment;
+        this.dueDate = dueDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+}
---
+++ src/main/java/entities/TaskType.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TaskType implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+
+    public TaskType() {
+    }
+
+    public TaskType(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
